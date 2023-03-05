a9e17d99bba648ab93255c3bfa9dc61ef5b08678
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-03-05 06:35:25 +0000

    hero section fininshed with fixed bugs

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,13 +4,12 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    <mesh>
+    <mesh className='bg-red-500'>
       <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -19,10 +18,11 @@ const Computers = () => {
         castShadow
         shadow-mapSize={1024}
       />
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={1.25}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -30,11 +30,32 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // add listener
+    const mediaQuery = window.matchMedia("(max-width:500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMedisaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // call back added to listner
+    mediaQuery.addEventListener("change", handleMedisaQueryChange);
+    return () => {
+      // remove when unmount compoent 
+      mediaQuery.removeEventListener("change", handleMedisaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameLoop="demand"
-      shadow
-      camera={{ position: [20, 3, 5], fav: 25 }}
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -44,7 +65,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
